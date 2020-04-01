433a75516e77b5ffac3fbca13d008ca422b74298
Vincent <git@vincenttunru.com>
2020-04-01 13:02:26 +0200

    Add fetching of Turtle Documents

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-export function add(a: number, b: number): number {
-  return a + b;
-}
+import { DatasetCore } from "rdf-js";
+
+export { fetchDocument } from "./document";
+
+/**
+ * Alias to indicate where we expect an IRI
+ */
+export type Reference = string;
+
+export type DocumentDataset = DatasetCore;
+export type DocumentDatasetWithMetadata = DocumentDataset & {
+  // Metadata properties
+};
