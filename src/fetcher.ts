6cf1e3b4985d266c08d309b21e66a71f3e7cf3b8
Vincent <git@vincenttunru.com>
2020-04-22 13:18:16 +0200

    Make the dynamic import work in Webpack
    Webpack can only inject modules when import calls have an explicit
    module name as parameter; modules are resolved statically during
    compilation, rather than dynamically at runtime.

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -2,14 +2,24 @@
  * @ignore Internal fallback for when no fetcher is provided; not to be used downstream.
  */
 export const fetch: typeof window.fetch = (resource, init) => {
-  const fetcherPromise = importFetcher("solid-auth-fetcher")
-    .catch(() => importFetcher("solid-auth-client"))
-    .catch(() => importFetcher("cross-fetch"));
+  // Implementation note: it's up to the client application to resolve these module names to the
+  // respective npm packages. At least one commonly used tool (Webpack) is only able to do that if
+  // the module names are literal strings.
+  const fetcherPromise = import(
+    // TypeScript cannot find the module - which is correct, since the consumer app should/can
+    // provide it:
+    // @ts-ignore
+    "solid-auth-fetcher"
+  )
+    .catch(() =>
+      import(
+        // TypeScript cannot find the module - which is correct, since the consumer app should/can
+        // provide it:
+        // @ts-ignore
+        "solid-auth-client"
+      )
+    )
+    .catch(() => import("cross-fetch"));
 
-  return fetcherPromise.then((fetch) => fetch(resource, init));
+  return fetcherPromise.then(({ fetch }) => fetch(resource, init));
 };
-
-async function importFetcher(moduleName: string): Promise<typeof window.fetch> {
-  const importedFetcher: typeof window.fetch = (await import(moduleName)).fetch;
-  return importedFetcher;
-}
