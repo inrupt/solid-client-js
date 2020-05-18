e49e933ea607659b1c1e3c33411540435c2f0807
Vincent <git@vincenttunru.com>
2020-05-18 18:21:56 +0200

    Make use of @jest/globals
    This makes it easier for TypeScript to typecheck our tests, rather
    relying on `any`-typed globals.
    
    For more details, see
    https://jestjs.io/blog/2020/05/05/jest-26#a-new-way-to-consume-jest---jestglobals
    
    Unfortunately, it was not possible to
    `import { jest } from "@jest/globals"` in files that also call
    jest.mock(). This was reported here:
    https://github.com/facebook/jest/issues/10060

--- src/datatypes.test.ts
+++ src/datatypes.test.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from "@jest/globals";
 import { DataFactory } from "n3";
 import { LocalNode } from "./index";
 import {
--- src/fetcher.test.ts
+++ src/fetcher.test.ts
@@ -1,9 +1,13 @@
+import { it, expect } from "@jest/globals";
 jest.mock("cross-fetch");
 
 import { fetch } from "./fetcher";
 
 it("should fallback to cross-fetch if no Solid-specific fetcher is available", async () => {
-  const crossFetch = jest.requireMock("cross-fetch");
+  const crossFetch = jest.requireMock("cross-fetch") as jest.Mock<
+    ReturnType<typeof window.fetch>,
+    [RequestInfo, RequestInit]
+  >;
 
   await fetch("https://some.url");
 
