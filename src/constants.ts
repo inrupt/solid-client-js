7b2ddacf2beb1a1a741429ecbe73f7e27785e199
Vincent <git@vincenttunru.com>
2020-05-26 14:12:50 +0200

    Add getAclRules()

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+// TODO: These should be replaced by auto-generated constants,
+//       if we can ensure that unused constants will be excluded from bundles.
+
+export const acl = {
+  Authorization: "http://www.w3.org/ns/auth/acl#Authorization",
+  mode: "http://www.w3.org/ns/auth/acl#mode",
+} as const;
+
+export const rdf = {
+  type: "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+} as const;
