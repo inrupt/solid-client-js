a91ed0108fbdd488e79d84f00337867ada72f1c7
Nicolas Seydoux <nseydoux@inrupt.com>
2020-06-02 18:22:29 +0200

    Brought the non-rdf fetching closer to the fetch API
    In order to not require a documentation additional to the fetch API's, the non-RDF fetching will be closer to the original fetch.

--- src/nonRdfData.ts
+++ src/nonRdfData.ts
@@ -1,6 +1,6 @@
 import { fetch } from "./fetcher";
 
-interface GetFileOptions {
+interface GetFileOptions extends RequestInit {
   fetch: typeof window.fetch;
   init: RequestInit;
 }
@@ -21,7 +21,7 @@ export async function fetchFile(
 ): Promise<Response> {
   const config = {
     ...defaultGetFileOptions,
-    ...options,
+    ...init,
   };
   return config.fetch(input, config.init);
 }
