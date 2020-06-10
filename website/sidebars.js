d652b0bf758afa9dc4decf817560c7319febadba
Vincent <git@vincenttunru.com>
2020-06-10 13:14:13 +0200

    Add docs on reading agent access data

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,11 +1,16 @@
 module.exports = {
   // The API sidebar will be automatically generated when running TypeDoc:
   api: {},
-  tutorials: [
+  prose: [
     {
       type: "category",
       label: "Tutorials",
-      items: ["getting-started"],
+      items: ["getting-started", "tutorials/managing-access"],
+    },
+    {
+      type: "category",
+      label: "Other",
+      items: ["glossary"],
     },
   ],
 };
