dedec360012bb2904ac9dd8be4684968c67cf753
Kay Kim <kay.kim@inrupt.com>
2020-08-19 20:37:07 -0400

    Exclude include dir from html build

--- docs/prose/conf.py
+++ docs/prose/conf.py
@@ -70,7 +70,9 @@ templates_path = ['./_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['./source/reference/api']
+
+# Exclude creating an html page for .rst files in includes directory as they are to be common content - not independent pages
+exclude_patterns = ['includes']
 
 # -- Options for HTML output -------------------------------------------------
 
