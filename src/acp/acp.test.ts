cade50a64c0a91046f8e9348337b30a706621e19
Vincent <git@vincenttunru.com>
2020-10-08 17:15:56 +0200

    Add getSolidDatasetWithAcp()

---
+++ src/acp/acp.test.ts
@@ -0,0 +1,300 @@
+/**
+ * Copyright 2020 Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { jest, describe, it, expect } from "@jest/globals";
+
+jest.mock("../fetcher.ts", () => ({
+  fetch: jest.fn(window.fetch).mockResolvedValue(
+    new Response(undefined, {
+      headers: { Location: "https://arbitrary.pod/resource" },
+    })
+  ),
+}));
+
+import { Response } from "cross-fetch";
+import { getSolidDatasetWithAcp, WithAccessibleAcr } from "./acp";
+import { acp, rdf } from "../constants";
+import * as SolidDatasetModule from "../resource/solidDataset";
+import { mockSolidDatasetFrom } from "../resource/mock";
+import { UrlString, WithResourceInfo } from "../interfaces";
+import { AccessControlResource } from "./control";
+import { createThing, setThing } from "../thing/thing";
+import { addIri } from "../thing/add";
+
+const defaultMockPolicies = {
+  policies: ["https://some.pod/policies#policy"],
+  memberPolicies: ["https://some.pod/policies#memberPolicy"],
+};
+function mockAcr(accessTo: UrlString, policies = defaultMockPolicies) {
+  let control = createThing({ name: "access-control" });
+  control = addIri(control, rdf.type, acp.AccessControl);
+  policies.policies.forEach((policyUrl) => {
+    control = addIri(control, acp.apply, policyUrl);
+  });
+  policies.memberPolicies.forEach((policyUrl) => {
+    control = addIri(control, acp.applyMembers, policyUrl);
+  });
+
+  let acr: AccessControlResource &
+    WithResourceInfo = Object.assign(
+    mockSolidDatasetFrom(accessTo + "?ext=acr"),
+    { accessTo: accessTo }
+  );
+  acr = setThing(acr, control);
+
+  return acr;
+}
+
+describe("getSolidDatasetWithAcp", () => {
+  it("calls the included fetcher by default", async () => {
+    const mockedFetcher = jest.requireMock("../fetcher.ts") as {
+      fetch: jest.Mock<
+        ReturnType<typeof window.fetch>,
+        [RequestInfo, RequestInit?]
+      >;
+    };
+
+    await getSolidDatasetWithAcp("https://some.pod/resource");
+
+    expect(mockedFetcher.fetch.mock.calls[0][0]).toEqual(
+      "https://some.pod/resource"
+    );
+  });
+
+  it("uses the given fetcher if provided", async () => {
+    const mockFetch = jest.fn(window.fetch).mockResolvedValue(new Response());
+
+    await getSolidDatasetWithAcp("https://some.pod/resource", {
+      fetch: mockFetch,
+    });
+
+    expect(mockFetch.mock.calls[0][0]).toEqual("https://some.pod/resource");
+  });
+
+  it("returns null for the ACR if it is not accessible to the current user", async () => {
+    const mockFetch = jest
+      .fn(window.fetch)
+      .mockResolvedValueOnce(
+        new Response(undefined, {
+          headers: {
+            Link: `<https://some.pod/acr.ttl>; rel="${acp.accessControl}"`,
+          },
+          url: "https://some.pod/resource",
+        } as ResponseInit)
+      )
+      .mockResolvedValueOnce(new Response("Not allowed", { status: 401 }));
+
+    const fetchedDataset = await getSolidDatasetWithAcp(
+      "https://some.pod/resource",
+      { fetch: mockFetch }
+    );
+
+    expect(mockFetch.mock.calls[0][0]).toEqual("https://some.pod/resource");
+    expect(mockFetch.mock.calls[1][0]).toEqual("https://some.pod/acr.ttl");
+    expect(fetchedDataset.internal_acp.acr).toBeNull();
+  });
+
+  it("returns an empty Object if no APRs were referenced", async () => {
+    const mockedSolidDataset = mockSolidDatasetFrom(
+      "https://arbitrary.pod/resource"
+    );
+    mockedSolidDataset.internal_resourceInfo.linkedResources = {
+      [acp.accessControl]: ["https://arbitrary.pod/resource?ext=acr"],
+    };
+    const mockedAcr = mockAcr("https://arbitrary.pod/resource", {
+      policies: [],
+      memberPolicies: [],
+    });
+    const mockedGetSolidDataset = jest.spyOn(
+      SolidDatasetModule,
+      "getSolidDataset"
+    );
+    mockedGetSolidDataset
+      .mockResolvedValueOnce(mockedSolidDataset)
+      .mockResolvedValueOnce(mockedAcr);
+
+    const fetchedDataset = await getSolidDatasetWithAcp(
+      "https://some.pod/resource"
+    );
+
+    expect(fetchedDataset.internal_acp.acr).not.toBeNull();
+    expect((fetchedDataset as WithAccessibleAcr).internal_acp.aprs).toEqual({});
+  });
+
+  it("fetches all referenced ACPs once", async () => {
+    const mockedSolidDataset = mockSolidDatasetFrom(
+      "https://some.pod/resource"
+    );
+    mockedSolidDataset.internal_resourceInfo.linkedResources = {
+      [acp.accessControl]: ["https://some.pod/resource?ext=acr"],
+    };
+    const mockedAcr = mockAcr("https://some.pod/resource", {
+      policies: ["https://some.pod/policy-resource#a-policy"],
+      memberPolicies: [
+        "https://some.pod/policy-resource#a-member-policy",
+        "https://some.pod/policy-resource#another-member-policy",
+      ],
+    });
+    const mockedAcp = mockSolidDatasetFrom("https://some.pod/policy-resource");
+    const mockedGetSolidDataset = jest.spyOn(
+      SolidDatasetModule,
+      "getSolidDataset"
+    );
+    mockedGetSolidDataset
+      .mockResolvedValueOnce(mockedSolidDataset)
+      .mockResolvedValueOnce(mockedAcr)
+      .mockResolvedValueOnce(mockedAcp);
+
+    const fetchedDataset = await getSolidDatasetWithAcp(
+      "https://some.pod/resource"
+    );
+
+    expect(mockedGetSolidDataset.mock.calls).toHaveLength(3);
+    expect(mockedGetSolidDataset.mock.calls[0][0]).toBe(
+      "https://some.pod/resource"
+    );
+    expect(mockedGetSolidDataset.mock.calls[1][0]).toBe(
+      "https://some.pod/resource?ext=acr"
+    );
+    expect(mockedGetSolidDataset.mock.calls[2][0]).toBe(
+      "https://some.pod/policy-resource"
+    );
+    expect(fetchedDataset.internal_acp.acr).not.toBeNull();
+    expect(
+      (fetchedDataset as WithAccessibleAcr).internal_acp.aprs[
+        "https://some.pod/policy-resource"
+      ]
+    ).toBeDefined();
+    expect(
+      (fetchedDataset as WithAccessibleAcr).internal_acp.aprs[
+        "https://some.pod/policy-resource"
+      ]
+    ).not.toBeNull();
+  });
+
+  it("lists Access Policy Resources that could not be fetched as null", async () => {
+    const mockedSolidDataset = mockSolidDatasetFrom(
+      "https://some.pod/resource"
+    );
+    mockedSolidDataset.internal_resourceInfo.linkedResources = {
+      [acp.accessControl]: ["https://some.pod/resource?ext=acr"],
+    };
+    const mockedAcr = mockAcr("https://some.pod/resource", {
+      policies: ["https://some.pod/policy-resource#a-policy"],
+      memberPolicies: [
+        "https://some.pod/policy-resource#a-member-policy",
+        "https://some.pod/policy-resource#another-member-policy",
+      ],
+    });
+    const mockedGetSolidDataset = jest.spyOn(
+      SolidDatasetModule,
+      "getSolidDataset"
+    );
+    mockedGetSolidDataset
+      .mockResolvedValueOnce(mockedSolidDataset)
+      .mockResolvedValueOnce(mockedAcr)
+      .mockRejectedValueOnce(
+        new Error("Could not fetch this Access Policy Resource")
+      );
+
+    const fetchedDataset = await getSolidDatasetWithAcp(
+      "https://some.pod/resource"
+    );
+
+    expect(mockedGetSolidDataset.mock.calls).toHaveLength(3);
+    expect(fetchedDataset.internal_acp.acr).not.toBeNull();
+    expect(
+      (fetchedDataset as WithAccessibleAcr).internal_acp.aprs[
+        "https://some.pod/policy-resource"
+      ]
+    ).toBeNull();
+  });
+
+  it("does not add the ACR itself to the APR list", async () => {
+    const mockedSolidDataset = mockSolidDatasetFrom(
+      "https://some.pod/resource"
+    );
+    mockedSolidDataset.internal_resourceInfo.linkedResources = {
+      [acp.accessControl]: ["https://some.pod/resource?ext=acr"],
+    };
+    const mockedAcr = mockAcr("https://some.pod/resource", {
+      policies: ["https://some.pod/resource?ext=acr#a-policy"],
+      memberPolicies: [
+        "https://some.pod/resource?ext=acr#a-member-policy",
+        "https://some.pod/resource?ext=acr#another-member-policy",
+      ],
+    });
+    const mockedGetSolidDataset = jest.spyOn(
+      SolidDatasetModule,
+      "getSolidDataset"
+    );
+    mockedGetSolidDataset
+      .mockResolvedValueOnce(mockedSolidDataset)
+      .mockResolvedValueOnce(mockedAcr);
+
+    const fetchedDataset = await getSolidDatasetWithAcp(
+      "https://some.pod/resource"
+    );
+
+    expect(mockedGetSolidDataset.mock.calls).toHaveLength(2);
+    expect(fetchedDataset.internal_acp.acr).not.toBeNull();
+    expect(
+      (fetchedDataset as WithAccessibleAcr).internal_acp.aprs[
+        "https://some.pod/resource?ext=acr"
+      ]
+    ).not.toBeDefined();
+  });
+
+  it("does not add the SolidDataset itself to the APR list", async () => {
+    const mockedSolidDataset = mockSolidDatasetFrom(
+      "https://some.pod/resource"
+    );
+    mockedSolidDataset.internal_resourceInfo.linkedResources = {
+      [acp.accessControl]: ["https://some.pod/resource?ext=acr"],
+    };
+    const mockedAcr = mockAcr("https://some.pod/resource", {
+      policies: ["https://some.pod/resource#a-policy"],
+      memberPolicies: [
+        "https://some.pod/resource#a-member-policy",
+        "https://some.pod/resource#another-member-policy",
+      ],
+    });
+    const mockedGetSolidDataset = jest.spyOn(
+      SolidDatasetModule,
+      "getSolidDataset"
+    );
+    mockedGetSolidDataset
+      .mockResolvedValueOnce(mockedSolidDataset)
+      .mockResolvedValueOnce(mockedAcr);
+
+    const fetchedDataset = await getSolidDatasetWithAcp(
+      "https://some.pod/resource"
+    );
+
+    expect(mockedGetSolidDataset.mock.calls).toHaveLength(2);
+    expect(fetchedDataset.internal_acp.acr).not.toBeNull();
+    expect(
+      (fetchedDataset as WithAccessibleAcr).internal_acp.aprs[
+        "https://some.pod/resource"
+      ]
+    ).not.toBeDefined();
+  });
+});
---
+++ src/acp/acp.ts
@@ -0,0 +1,174 @@
+/**
+ * Copyright 2020 Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { acp } from "../constants";
+import {
+  internal_toIriString,
+  SolidDataset,
+  Url,
+  UrlString,
+  WithResourceInfo,
+} from "../interfaces";
+import {
+  getSourceUrl,
+  internal_defaultFetchOptions,
+} from "../resource/resource";
+import { getSolidDataset } from "../resource/solidDataset";
+import {
+  AccessControlResource,
+  getAccessControlAll,
+  getMemberPolicyUrlAll,
+  getPolicyUrlAll,
+  hasLinkedAcr,
+} from "./control";
+import { PolicyDataset } from "./policy";
+
+/**
+ * ```{note} The Web Access Control specification is not yet finalised. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * Fetch a SolidDataset, its associated Access Control Resource (if available to the current user),
+ * and all the Access Control Policies referred to therein, if available to the current user.
+ *
+ * @param url URL of the SolidDataset to fetch.
+ * @param options Optional parameter `options.fetch`: An alternative `fetch` function to make the HTTP request, compatible with the browser-native [fetch API](https://developer.mozilla.org/docs/Web/API/WindowOrWorkerGlobalScope/fetch#parameters).
+ * @returns A SolidDataset and the ACR that applies to it, if available to the authenticated user, and the APRs that are referred to therein, if available to the authenticated user.
+ */
+export async function getSolidDatasetWithAcp(
+  url: Url | UrlString,
+  options: Partial<
+    typeof internal_defaultFetchOptions
+  > = internal_defaultFetchOptions
+): Promise<SolidDataset & WithAcp> {
+  const urlString = internal_toIriString(url);
+  const config = {
+    ...internal_defaultFetchOptions,
+    ...options,
+  };
+
+  const solidDataset = await getSolidDataset(urlString, config);
+  const acp = await fetchAcp(solidDataset, config);
+  return Object.assign(solidDataset, acp);
+}
+
+export type WithAcp = {
+  internal_acp:
+    | {
+        acr: AccessControlResource;
+        aprs: Record<UrlString, PolicyDataset | null>;
+      }
+    | {
+        acr: null;
+      };
+};
+export type WithAccessibleAcr = WithAcp & {
+  internal_acp: {
+    acr: Exclude<WithAcp["internal_acp"]["acr"], null>;
+  };
+};
+
+async function fetchAcp(
+  resource: WithResourceInfo,
+  options: Partial<typeof internal_defaultFetchOptions>
+): Promise<WithAcp> {
+  if (!hasLinkedAcr(resource)) {
+    return {
+      internal_acp: {
+        acr: null,
+      },
+    };
+  }
+  let acr: SolidDataset;
+  try {
+    acr = await getSolidDataset(
+      // Whereas a Resource can generally have multiple linked Resources for the same relation,
+      // it can only have one Access Control Resource for that ACR to be valid.
+      // Hence the accessing of [0] directly:
+      resource.internal_resourceInfo.linkedResources[acp.accessControl][0],
+      options
+    );
+  } catch (e: unknown) {
+    return {
+      internal_acp: {
+        acr: null,
+      },
+    };
+  }
+
+  const resourceUrl = getSourceUrl(resource);
+  const acrUrl = getSourceUrl(acr);
+  const acrDataset: AccessControlResource = Object.assign(acr, {
+    accessTo: getSourceUrl(resource),
+  });
+  const policyUrls = getReferencedPolicyUrls(acrDataset)
+    // Prevent the Resource itself, and the Access Control Resource, from being fetched again:
+    .filter((policyUrl) => ![resourceUrl, acrUrl].includes(policyUrl));
+  const policyDatasets = await Promise.all(
+    policyUrls.map((url) => fetchPolicyDataset(url, options))
+  );
+  const acpInfo: WithAccessibleAcr = {
+    internal_acp: {
+      acr: acrDataset,
+      aprs: {},
+    },
+  };
+  policyUrls.forEach((policyUrl, i) => {
+    acpInfo.internal_acp.aprs[policyUrl] = policyDatasets[i];
+  });
+  return acpInfo;
+}
+
+async function fetchPolicyDataset(
+  url: UrlString,
+  options: Partial<typeof internal_defaultFetchOptions>
+): Promise<PolicyDataset | null> {
+  try {
+    return await getSolidDataset(url, options);
+  } catch (e) {
+    // We expect fetching of Access Policy Resources to fail often,
+    // specifically when the current user does not have access to that Resource.
+    return null;
+  }
+}
+
+function getReferencedPolicyUrls(acr: AccessControlResource): UrlString[] {
+  const policyUrls: UrlString[] = [];
+
+  const controls = getAccessControlAll(acr);
+  controls.forEach((control) => {
+    policyUrls.push(...getPolicyUrlAll(control).map(getResourceUrl));
+    policyUrls.push(...getMemberPolicyUrlAll(control).map(getResourceUrl));
+  });
+
+  const uniqueUrls = Array.from(new Set(policyUrls));
+  return uniqueUrls;
+}
+
+/**
+ * To verify whether two URLs are at the same location, we need to strip the hash.
+ * This function does that.
+ */
+function getResourceUrl(urlWithHash: UrlString): UrlString {
+  const url = new URL(urlWithHash);
+  url.hash = "";
+  return url.href;
+}
