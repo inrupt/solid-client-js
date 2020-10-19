5e7f0a057d1c8b2b0b7884c3e0f3f8e559bc21f9
Nicolas Seydoux <nseydoux@inrupt.com>
2020-10-19 08:27:21 +0200

    Add functions to manage agents/groups in rules
    This adds a set of functions enabling manipulation of the agents a rule applies to. Additionnally to agents and groups identified by IRI, a couple of functions are dedicated to manipulating specific agent classes, namely authenticated agents and public agents.

--- src/acp/rule.ts
+++ src/acp/rule.ts
@@ -19,12 +19,26 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import { acp } from "../constants";
-import { SolidDataset, ThingPersisted, UrlString } from "../interfaces";
+import { age } from "rdf-namespaces/dist/foaf";
+import { acp, rdf } from "../constants";
+import {
+  internal_toIriString,
+  SolidDataset,
+  ThingPersisted,
+  Url,
+  UrlString,
+  WebId,
+} from "../interfaces";
+import { internal_defaultFetchOptions } from "../resource/resource";
+import {
+  createSolidDataset,
+  saveSolidDatasetAt,
+} from "../resource/solidDataset";
 import { addIri } from "../thing/add";
-import { getIriAll } from "../thing/get";
+import { getIri, getIriAll, getUrl } from "../thing/get";
 import { removeAll, removeIri } from "../thing/remove";
-import { setIri } from "../thing/set";
+import { setIri, setUrl } from "../thing/set";
+import { cloneThing, createThing, getThing, setThing } from "../thing/thing";
 import { Policy } from "./policy";
 
 export type RuleDataset = SolidDataset;
@@ -232,4 +246,284 @@ export function setForbiddenRuleOnPolicy(policy: Policy, rule: Rule): Policy {
  */
 export function getForbiddenRuleOnPolicyAll(policy: Policy): UrlString[] {
   return getIriAll(policy, acp.noneOf);
+}
+
+/**
+ * ```{note} There is no Access Control Policies specification yet. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * Initialise a new empty [[SolidDataset]] to store [[Rule]]'s in.
+ */
+export const createRuleDataset = createSolidDataset;
+
+/**
+ * ```{note} There is no Access Control Policies specification yet. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * Initialise a new, empty [[Rule]].
+ *
+ * @param url URL that identifies this [[Rule]].
+ */
+export function createRule(url: Url | UrlString): Rule {
+  const stringUrl = internal_toIriString(url);
+  let ruleThing = createThing({ url: stringUrl });
+  ruleThing = setUrl(ruleThing, rdf.type, acp.Rule);
+  return ruleThing;
+}
+
+/**
+ * ```{note} There is no Access Control Policies specification yet. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * Get the [[Rule]] with the given URL from an [[RuleDataset]].
+ *
+ * @param ruleResource The Resource that contains the given [[Rule]].
+ * @param url URL that identifies this [[Rule]].
+ * @returns The requested [[Rule]], if it exists, or `null` if it does not.
+ */
+export function getRule(
+  ruleResource: RuleDataset,
+  url: Url | UrlString
+): Policy | null {
+  const foundThing = getThing(ruleResource, url);
+  if (foundThing === null || getUrl(foundThing, rdf.type) !== acp.Rule) {
+    return null;
+  }
+  return foundThing;
+}
+
+/**
+ * ```{note} There is no Access Control Policies specification yet. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * List all the agents a [[Rule]] applies **directly** to. This will not include agents
+ * that are part of a group the [[Rule]] applies to, nor will it include specific agent
+ * classes, such as authenticated or public agents.
+ *
+ * @param rule The rule from which agents are read.
+ * @returns A list of the WebIDs of agents included in the rule.
+ * @since Unreleased
+ */
+export function getAgentForRuleAll(rule: Rule): WebId[] {
+  return getIriAll(rule, acp.agent).filter(
+    (agent: string) =>
+      agent !== acp.PublicAgent && agent !== acp.AuthenticatedAgent
+  );
+}
+
+/**
+ * ```{note} There is no Access Control Policies specification yet. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * Overrite the agents the [[Rule]] applies to with the provided agents.
+ *
+ * @param rule The rule for which agents are set.
+ * @param agents The list of agents the rule should apply to.
+ * @returns A copy of the input rule, applying to a different set of agents.
+ * @since Unreleased
+ */
+export function setAgentInRule(rule: Rule, agents: WebId[]): Rule {
+  // Preserve the special agent classes authenticated and public, which we
+  // don't want to overwrite with this function.
+  // NB: This raises the question wether the named agents and agent classes should
+  // be linked to with the same predicate.
+  const isPublic = hasPublicInRule(rule);
+  const isAuthenticated = hasAuthenticatedInRule(rule);
+  let result = cloneThing(rule);
+  if (agents.length === 0) {
+    result = removeAll(rule, acp.agent);
+  } else {
+    result = setIri(rule, acp.agent, agents[0]);
+    agents.slice(1).forEach((agent) => {
+      result = addIri(result, acp.agent, agent);
+    });
+  }
+  // Restore public and authenticated
+  result = setPublicForRule(result, isPublic);
+  result = setAuthenticatedForRule(result, isAuthenticated);
+  return result;
+}
+
+/**
+ * ```{note} There is no Access Control Policies specification yet. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * Apply the [[Rule]] to an additional agent.
+ *
+ * @param rule The [[Rule]] to be applied to an additional agent.
+ * @param agent The agent the [[Rule]] should apply to.
+ * @returns A copy of the [[Rule]], applying to an additional agent.
+ * @since Unreleased
+ */
+export function addAgentToRule(rule: Rule, agent: WebId): Rule {
+  return addIri(rule, acp.agent, agent);
+}
+
+/**
+ * ```{note} There is no Access Control Policies specification yet. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * Prevent the [[Rule]] from applying to a given agent directly. This will not
+ * remove the agent from any groups the rule applies to.
+ *
+ * @param rule The [[Rule]] that should no longer apply to a given agent.
+ * @param agent The agent the rule should no longer apply to.
+ * @returns A copy of the rule, not applying to the given agent.
+ * @since Unreleased
+ */
+export function removeAgentFromRule(rule: Rule, agent: WebId): Rule {
+  return removeIri(rule, acp.agent, agent);
+}
+
+/**
+ * ```{note} There is no Access Control Policies specification yet. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * Lists all the groups a [[Rule]] applies to.
+ *
+ * @param rule The rule from which groups are read.
+ * @returns A list of the [[URL]]'s of groups included in the rule.
+ * @since Unreleased
+ */
+export function getGroupForRuleAll(rule: Rule): UrlString[] {
+  return getIriAll(rule, acp.group);
+}
+
+/**
+ * ```{note} There is no Access Control Policies specification yet. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * Overrite the groups the [[Rule]] applies to with the provided groups.
+ *
+ * @param rule The rule for which groups are set.
+ * @param agents The list of groups the rule should apply to.
+ * @returns A copy of the input rule, applying to a different set of groups.
+ * @since Unreleased
+ */
+export function setGroupInRule(rule: Rule, groups: UrlString[]): Rule {
+  if (groups.length === 0) {
+    return removeAll(rule, acp.group);
+  }
+  let result = setIri(rule, acp.group, groups[0]);
+  groups.slice(1).forEach((group: UrlString) => {
+    result = addIri(result, acp.group, group);
+  });
+  return result;
+}
+
+/**
+ * ```{note} There is no Access Control Policies specification yet. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * Apply the [[Rule]] to an additional group.
+ *
+ * @param rule The [[Rule]] to be applied to an additional group.
+ * @param agent The group the [[Rule]] should apply to.
+ * @returns A copy of the [[Rule]], applying to an additional group.
+ * @since Unreleased
+ */
+export function addGroupToRule(rule: Rule, group: UrlString): Rule {
+  return addIri(rule, acp.group, group);
+}
+
+/**
+ * ```{note} There is no Access Control Policies specification yet. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * Prevent the [[Rule]] from applying to a given group.
+ *
+ * @param rule The [[Rule]] that should no longer apply to a given group.
+ * @param agent The group the rule should no longer apply to.
+ * @returns A copy of the rule, not applying to the given group.
+ * @since Unreleased
+ */
+export function removeGroupFromRule(rule: Rule, group: UrlString): Rule {
+  return removeIri(rule, acp.group, group);
+}
+
+/**
+ * ```{note} There is no Access Control Policies specification yet. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * Check if the rule applies to any agent.
+ *
+ * @param rule The rule checked for public access.
+ * @returns Whether the rule applies to any agent or not.
+ */
+export function hasPublicInRule(rule: Rule): boolean {
+  return (
+    getIriAll(rule, acp.agent).filter((agent) => agent === acp.PublicAgent)
+      .length > 0
+  );
+}
+
+/**
+ * ```{note} There is no Access Control Policies specification yet. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * Enable or disable a rule from applying to any agent.
+ *
+ * @param rule The rule being modified.
+ * @param hasPublic A boolean indicating whether the rule should apply or not to any agent.
+ * @returns A copy of the rule, updated to apply/not apply to any agent.
+ * @status Unreleased
+ */
+export function setPublicForRule(rule: Rule, hasPublic: boolean): Rule {
+  if (!hasPublic) {
+    return removeIri(rule, acp.agent, acp.PublicAgent);
+  }
+  return addIri(rule, acp.agent, acp.PublicAgent);
+}
+
+/**
+ * ```{note} There is no Access Control Policies specification yet. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * Check if the rule applies to any authenticated agent.
+ *
+ * @param rule The rule checked for authenticated access.
+ * @returns Whether the rule applies to any authenticated agent or not.
+ */
+export function hasAuthenticatedInRule(rule: Rule): boolean {
+  return (
+    getIriAll(rule, acp.agent).filter(
+      (agent) => agent === acp.AuthenticatedAgent
+    ).length > 0
+  );
+}
+
+/**
+ * ```{note} There is no Access Control Policies specification yet. As such, this
+ * function is still experimental and subject to change, even in a non-major release.
+ * ```
+ *
+ * Enable or disable a rule from applying to any authenticated agent.
+ *
+ * @param rule The rule being modified.
+ * @param hasPublic A boolean indicating whether the rule should apply or not to any authenticated agent.
+ * @returns A copy of the rule, updated to apply/not apply to any authenticated agent.
+ * @status Unreleased
+ */
+export function setAuthenticatedForRule(
+  rule: Rule,
+  authenticated: boolean
+): Rule {
+  if (!authenticated) {
+    return removeIri(rule, acp.agent, acp.AuthenticatedAgent);
+  }
+  return addIri(rule, acp.agent, acp.AuthenticatedAgent);
 }
