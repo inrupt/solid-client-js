d43d1a6b360b14f4e229056923de10dcb37af996
Vincent <git@vincenttunru.com>
2020-11-12 12:37:49 +0100

    Workaround for N3's import
    This is required to make it work in Node with ES module support,
    where it only has a default export (because it is published as a
    CommonJS module there), and in Webpack, where it only has named
    exports (because it is published as an ES module there).
    
    Since dynamic imports always return a single object (with the
    named exports as properties for ES modules, and with a `default`
    property containing all exports for CommonJS modules), we can use
    that to import the module and then dynamically determine whether
    we have to return the `default` property or the imported object
    itself.
    
    Unfortunately, that does mean that tree-shaking N3 does not work,
    until N3 makes itself compatible with both Node's module systems
    and the ES imports from Webpack and co:
    https://github.com/rdfjs/N3.js/issues/196

--- src/formats/turtle.ts
+++ src/formats/turtle.ts
@@ -19,7 +19,6 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import { Writer, Parser } from "n3";
 import { Quad } from "rdf-js";
 import { IriString } from "../interfaces";
 import { DataFactory } from "../rdfjs";
@@ -29,8 +28,9 @@ import { DataFactory } from "../rdfjs";
  * @internal Utility method for internal use; not part of the public API.
  */
 export async function triplesToTurtle(quads: Quad[]): Promise<string> {
+  const n3 = await loadN3();
   const format = "text/turtle";
-  const writer = new Writer({ format: format });
+  const writer = new n3.Writer({ format: format });
   // Remove any potentially lingering references to Named Graphs in Quads;
   // they'll be determined by the URL the Turtle will be sent to:
   const triples = quads.map((quad) =>
@@ -60,7 +60,8 @@ export async function turtleToTriples(
   resourceIri: IriString
 ): Promise<Quad[]> {
   const format = "text/turtle";
-  const parser = new Parser({ format: format, baseIRI: resourceIri });
+  const n3 = await loadN3();
+  const parser = new n3.Parser({ format: format, baseIRI: resourceIri });
 
   const parsingPromise = new Promise<Quad[]>((resolve, reject) => {
     const parsedTriples: Quad[] = [];
@@ -77,4 +78,28 @@ export async function turtleToTriples(
   });
 
   return parsingPromise;
+}
+
+async function loadN3() {
+  // When loaded via Webpack or another bundler that looks at the `modules` field in package.json,
+  // N3 serves up ES modules with named exports.
+  // However, when it is loaded in Node, it serves up a CommonJS module, which, when imported from
+  // a Node ES module, is in the shape of a default export that is an object with all the named
+  // exports as its properties.
+  // This means that if we were to import the default module, our code would fail in Webpack,
+  // whereas if we imported the named exports, our code would fail in Node.
+  // As a workaround, we use a dynamic import. This way, we can use the same syntax in every
+  // environment, where the differences between the environments are in whether the returned object
+  // includes a `default` property that contains all exported functions, or whether those functions
+  // are available on the returned object directly. We can then respond to those different
+  // situations at runtime.
+  // Unfortunately, that does mean that tree shaking will not work until N3 also provides ES modules
+  // for Node, or adds a default export for Webpack. See
+  // https://github.com/rdfjs/N3.js/issues/196
+  const n3Module = await import("n3");
+  /* istanbul ignore if: the package provides named exports in the unit test environment */
+  if (typeof n3Module.default !== "undefined") {
+    return n3Module.default;
+  }
+  return n3Module;
 }
