af4776beb5d9a90223fab85632e5a4b1418e796c
Vincent <git@vincenttunru.com>
2021-01-13 10:59:04 +0100

    WIP: Skeleton for hasInaccessiblePolicies, for Nicolas to potentially build on

---
+++ src/access/acp.ts
@@ -0,0 +1,37 @@
+/**
+ * Copyright 2020 Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { WithAccessibleAcr } from "../acp/acp";
+import { getPolicyUrlAll } from "../acp/control";
+import { WithResourceInfo } from "../interfaces";
+import { getSourceIri } from "../resource/resource";
+
+export function internal_hasInaccessiblePolicies(
+  resource: WithAccessibleAcr & WithResourceInfo
+): boolean {
+  const sourceIri = getSourceIri(resource);
+  const policyUrls = getPolicyUrlAll(resource);
+  return (
+    policyUrls.findIndex(
+      (url) => url.substring(0, sourceIri.length) !== sourceIri
+    ) === -1
+  );
+}
