a09ae68108f37b11b96c8fa4f7c2f0fe5a924b94
Vincent <git@vincenttunru.com>
2021-02-17 11:20:58 +0100

    Don't inject Jest globals in end-to-end tests

--- src/e2e-node/e2e.test.ts
+++ src/e2e-node/e2e.test.ts
@@ -19,10 +19,7 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-/**
- * @jest-environment node
- */
-
+import { jest, describe, it } from "@jest/globals";
 import { foaf, schema } from "rdf-namespaces";
 import { Session } from "@inrupt/solid-client-authn-node";
 import { config } from "dotenv-flow";
