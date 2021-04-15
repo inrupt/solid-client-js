1d25733e0c60b56be11b8f9827ded06e215acd22
Vincent <git@vincenttunru.com>
2021-04-15 17:04:48 +0200

    Change selectors for updated IDP screen

--- src/e2e-browser/pageModels/broker.ts
+++ src/e2e-browser/pageModels/broker.ts
@@ -23,40 +23,20 @@ import { t, ClientFunction, Selector } from "testcafe";
 import { screen } from "@testing-library/testcafe";
 
 export class BrokerPage {
-  accessOpenIdCheckbox: Selector;
-  accessOfflineCheckbox: Selector;
-  accessWebIdCheckbox: Selector;
-  rememberForeverRadioButton: Selector;
-  rememberOneHourRadioButton: Selector;
-  rememberNotRadioButton: Selector;
   authoriseButton: Selector;
   denyButton: Selector;
 
   constructor() {
-    this.accessOpenIdCheckbox = screen.getByLabelText(
-      "log in using your identity"
-    );
-    this.accessOfflineCheckbox = screen.getByLabelText("offline access");
-    this.accessWebIdCheckbox = screen.getByLabelText("solid webid");
-    this.rememberForeverRadioButton = screen.getByLabelText(
-      "remember this decision until I revoke it"
-    );
-    this.rememberOneHourRadioButton = screen.getByLabelText(
-      "remember this decision for one hour"
-    );
-    this.rememberNotRadioButton = screen.getByLabelText(
-      "prompt me again next time"
-    );
-    this.authoriseButton = screen.getByText("Authorize");
+    this.authoriseButton = screen.getByText("Approve");
     this.denyButton = screen.getByText("Deny");
   }
 
   async authoriseOnce() {
     await onAuthorisePage();
-    await t.click(this.rememberNotRadioButton).click(this.authoriseButton);
+    await t.click(this.authoriseButton);
   }
 }
 
 export async function onAuthorisePage() {
-  await t.expect(Selector("form[name=confirmationForm]").exists).ok();
+  await t.expect(Selector("form#approve").exists).ok();
 }
