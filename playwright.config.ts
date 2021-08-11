28ebae4a55bea6ae45e209cbf161456fd435ebb7
Vincent <git@vincenttunru.com>
2021-08-11 13:09:48 +0200

    Run browser-based E2E tests using Playwright
    TestCafe was getting pretty flaky.

---
+++ playwright.config.ts
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2021 Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+  testMatch: "*.playwright.ts",
+  retries: 1,
+  globalSetup: require.resolve("./src/e2e-browser/globalSetup.ts"),
+  use: {
+    baseURL: "http://localhost:1234",
+    headless: true,
+    screenshot: "only-on-failure",
+    trace: "on",
+    video: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "Firefox",
+      use: {
+        browserName: "firefox",
+        userAgent: `Browser-based solid-client end-to-end tests running ${
+          process.env.CI === "true" ? "in CI" : "locally"
+        }. Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0`,
+      },
+    },
+    {
+      name: "Chromium",
+      use: {
+        browserName: "chromium",
+        userAgent: `Browser-based solid-client end-to-end tests running ${
+          process.env.CI === "true" ? "in CI" : "locally"
+        }. Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36`,
+      },
+    },
+    {
+      name: "WebKit",
+      use: {
+        browserName: "webkit",
+        userAgent: `Browser-based solid-client end-to-end tests running ${
+          process.env.CI === "true" ? "in CI" : "locally"
+        }. Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1`,
+      },
+    },
+  ],
+};
+export default config;
---
+++ src/e2e-browser/e2e.playwright.ts
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2021 Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/* eslint-disable jest/no-done-callback */
+/**
+ * Copyright 2021 Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { test, expect } from "@playwright/test";
+import { config } from "dotenv-flow";
+
+// We re-use the test helpers from elsewhere, but we need to ignore the
+// TypeScript error (TS6059) that complains about not all files being under the
+// one 'rootDir'.
+// @ts-ignore
+import type { getHelpers } from "../../.codesandbox/sandbox/src/end-to-end-test-helpers";
+import { essUserLogin } from "./roles-playwright";
+
+// E2eHelpers is a global defined in .codesandbox/sandbox/src/end-to-end-helper.
+// Since code inside of page.evaluate is executed in that context in the browser,
+// that variable is available to it - but as far as TypeScript is concerned,
+// it is executed in the context of this test file.
+// Hence, we just declare this variable to be of the same type here.
+const E2eHelpers: ReturnType<typeof getHelpers> = {} as any;
+
+// Load environment variables from .env.test.local if available:
+config({
+  default_node_env: "test",
+  path: __dirname,
+  // In CI, actual environment variables will overwrite values from .env files.
+  // We don't need warning messages in the logs for that:
+  silent: process.env.CI === "true",
+});
+
+test("creating and removing empty Containers", async ({ page }) => {
+  const createContainer = () =>
+    page.evaluate(() => E2eHelpers.createContainer());
+  const deleteContainer = () =>
+    page.evaluate(() => E2eHelpers.deleteContainer());
+
+  await page.goto("/end-to-end-test.html");
+  await essUserLogin(page);
+  const createdContainer = await createContainer();
+  expect(createdContainer).not.toBeNull();
+  expect(createdContainer).toEqual(expect.objectContaining({}));
+  await deleteContainer();
+});
