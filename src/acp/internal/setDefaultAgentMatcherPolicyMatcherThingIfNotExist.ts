3fd819e022a9e3546b1d028f4fb181924d4fbe95
Matthieu Bosquet <matthieu@cognithive.com>
2021-11-24 12:31:33 +0000

    Describe setDefaultAgentMatcherPolicyMatcherThingIfNotExist

--- src/acp/internal/setDefaultAgentMatcherPolicyMatcherThingIfNotExist.ts
+++ src/acp/internal/setDefaultAgentMatcherPolicyMatcherThingIfNotExist.ts
@@ -40,7 +40,20 @@ import { setModes } from "./setModes";
 /** @hidden */
 export const DEFAULT_POLICY_MATCHER_PREDICATE = ACP.anyOf;
 
-/** @hidden */
+/**
+ * This functions scaffolds the default elements required for giving access to
+ * an agent:
+ * 1. If the Access Control Resource is empty, create the AccessControlResource
+ *    element;
+ * 2. If the current default Access Control doesn't exist (the one applying to
+ *    one of: the Resource, the ACR, the Member Resources or the ACR of members)
+ *    create it;
+ * 3. If the default Policy for allowing the Access Modes for the current
+ *    default Access Control doesn't exist, create it;
+ * 4. If the default "anyOf" Agent Matcher for the current Policy creates it;
+ * 5. Returns an ACR with a Matcher ready to add to.
+ * @hidden
+ * */
 export function setDefaultAgentMatcherPolicyMatcherThingIfNotExist<
   T extends WithAccessibleAcr
 >(resource: T, name: DefaultAccessControlName, mode: keyof AccessModes): T {
