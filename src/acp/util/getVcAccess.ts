3716caffb2620ae862bed3c871cd3cb19c8db598
Nicolas Seydoux <nseydoux@inrupt.com>
2021-11-28 10:26:52 +0100

    Add TSDoc to functions

--- src/acp/util/getVcAccess.ts
+++ src/acp/util/getVcAccess.ts
@@ -52,6 +52,26 @@ const linkExists = (
 // instead of checking at each link that it isn't null and it is connected to the
 // next link.
 
+/**
+ * ```{note}
+ * The ACP specification is a draft. As such, this function is experimental and
+ * subject to change, even in a non-major release.
+ * See also: https://solid.github.io/authorization-panel/acp-specification/
+ * ```
+ *
+ * Get the maximum access modes that are allowed for a VC holder for a given resource.
+ * If the resource owner issued an Access Grant for the resource, the agent that
+ * has been granted access will have at most the permissions returned by this function.
+ * The Access Grant may be more restrictive.
+ *
+ * Note that only the modes set using [[setVcAccess]] will be returned by this function.
+ * Additional access may have been set if the ACR has been manipulated not using this
+ * library, which is currently out of scope.
+ *
+ * @param resourceWithAcr The resource for which the VC access modes are looked up.
+ * @returns The access modes available to a VC holder.
+ * @since unreleased
+ */
 export function getVcAccess(resourceWithAcr: WithAccessibleAcr): AccessModes {
   const acr = internal_getAcr(resourceWithAcr);
 
--- src/acp/util/setVcAccess.ts
+++ src/acp/util/setVcAccess.ts
@@ -43,6 +43,28 @@ import { AccessModes } from "../type/AccessModes";
 export const DEFAULT_VC_POLICY_NAME = "defaultVcPolicy";
 export const DEFAULT_VC_MATCHER_NAME = "defaultVcMatcher";
 
+/**
+ * * ```{note}
+ * The ACP specification is a draft. As such, this function is experimental and
+ * subject to change, even in a non-major release.
+ * See also: https://solid.github.io/authorization-panel/acp-specification/
+ * ```
+ *
+ * Set the maximum access modes that are allowed for a VC holder for a given resource.
+ * If the resource owner issued an Access Grant for the resource, the agent that
+ * has been granted access will have at most the permissions set by this function.
+ * The Access Grant may be more restrictive.
+ *
+ * Note that additional access may have been set if the ACR has been manipulated
+ * not using this library, which is currently out of scope. In this case, the access
+ * set by this function may not apply.
+ *
+ * @param resourceWithAcr The resource for which the access modes are being set for VC holders.
+ * @param access The access modes to set. Setting a mode to `true` will enable it, to `false`
+ * will disable it, and to `undefined` will leave it unchanged compared to what was previously
+ * set.
+ * @returns A copy of the resource and its attached ACR, updated to the new access modes.
+ */
 export function setVcAccess(
   resourceWithAcr: WithAccessibleAcr,
   access: Partial<AccessModes>
