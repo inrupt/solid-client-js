90c2c2b70e6706f5e7a76f4ce34ee14768fb677e
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2021-12-01 21:46:59 +0100

    Fix ACP documentation note
    The extra * was causing sphinx to crash

--- src/acp/util/setVcAccess.ts
+++ src/acp/util/setVcAccess.ts
@@ -44,7 +44,7 @@ export const DEFAULT_VC_POLICY_NAME = "defaultVcPolicy";
 export const DEFAULT_VC_MATCHER_NAME = "defaultVcMatcher";
 
 /**
- * * ```{note}
+ * ```{note}
  * The ACP specification is a draft. As such, this function is experimental and
  * subject to change, even in a non-major release.
  * See also: https://solid.github.io/authorization-panel/acp-specification/
