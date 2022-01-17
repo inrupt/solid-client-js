d036f3529c88b6f6f30bd622928d56423db1824d
Nicolas Seydoux <nseydoux@inrupt.com>
2022-01-17 16:52:10 +0100

    Replace implicit transitive import
    @inrupt/solid-client depends on @rdfjs/dataset, which in turn depends on @rdfjs/data-model. However, @inrupt/solid-client had a direct import to the latter, which is incorrect since it isn't among the direct dependencies. It has been replaced by an import of the factory exposed by the former.

--- src/rdfjs.internal.ts
+++ src/rdfjs.internal.ts
@@ -19,7 +19,7 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import RdfJsDataFactory from "@rdfjs/data-model";
+import RdfJsDataFactory from "@rdfjs/dataset";
 import * as RdfJs from "@rdfjs/types";
 import {
   BlankNodeId,
