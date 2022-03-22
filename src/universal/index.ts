7e4451771c6dd8e8070d1c47d4c67aa9761c843d
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2022-03-22 22:21:16 +0100

    docs: include information on how to import the Universal Access APIs
    This also renames this module in the sidebar from 'universal' to 'universalAccess'.

--- src/universal/index.ts
+++ src/universal/index.ts
@@ -19,6 +19,50 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+/**
+ * :::{admonition} Experimental API
+ * :class: important
+ *
+ * This API is still experimental, and subject to change. It builds on top of
+ * both ACP and WAC, aiming at being adaptable to any Access Control system that
+ * may be implemented in Solid. That is why it is purely Resource-centric: the
+ * library discovers metadata associated with the Resource itself, and calls the
+ * appropriate underlying API to deal with the Access Control in place for the
+ * target Resource.
+ *
+ * As it is still under development, the following export is *only* intended for
+ * experimentation by early adopters, and is not recommended for production
+ * applications.
+ *
+ * For more information see: [Tutorial: Managing
+ * Access](https://docs.inrupt.com/developer-tools/javascript/client-libraries/tutorial/manage-access/)
+ * :::
+ *
+ * This module can be imported as an object from the main package, which results
+ * in tree-shaking not being supported (so all the exported APIs will likely end
+ * up in your bundle). This import style is used for environments such as nextjs
+ * or create-react-app.
+ *
+ * ```typescript
+ * import { universalAccess } from "@inrupt/solid-client";
+ * ```
+ *
+ * Alternatively, if your environment supports [export
+ * maps](https://nodejs.org/dist/latest-v16.x/docs/api/packages.html#exports),
+ * then you can import directly:
+ *
+ * ```typescript
+ * import * as universalAccess from "@inrupt/solid-client/universal";
+ * ```
+ *
+ * If you're using Typescript, and receive errors about type definitions not
+ * being found, please see this
+ * [documentation](https://www.typescriptlang.org/docs/handbook/esm-node.html)
+ *
+ * @packageDocumentation
+ * @module universalAccess
+ */
+
 export { getAclServerResourceInfo } from "./getAclServerResourceInfo";
 export { getAgentAccess } from "./getAgentAccess";
 export { getAgentAccessAll } from "./getAgentAccessAll";
