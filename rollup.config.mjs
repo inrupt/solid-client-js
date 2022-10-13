1053ad02ae1dbbee9b6a88f00e2ea0a7d6265a2a
Nicolas Seydoux <nseydoux@inrupt.com>
2022-10-13 10:46:14 +0200

    Move rollup config to ESM

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,12 @@
-import pkg from "./package.json";
+// The following is only possible from Node 18 onwards
+// import pkg from "./package.json" assert { type: "json" };
+
+// Until we only support Node 18+, this should be used instead
+// (see https://rollupjs.org/guide/en/#importing-packagejson) 
+import { createRequire } from 'node:module';
+const require = createRequire(import.meta.url);
+const pkg = require('./package.json');
+
 import typescript from "rollup-plugin-typescript2";
 
 export default {
