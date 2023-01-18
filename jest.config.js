289ad961720ebdf0124b4813dff3f4c7e498c3cb
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2023-01-19 00:58:12 +0100

    chore: testing out maxWorkers for faster test runs

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
+const CPUs = require("os").cpus().length;
+
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   testRegex: "/src/.*\\.test\\.ts$",
   clearMocks: true,
+  // Increase workers on CI:
+  maxWorkers: process.env.CI ? CPUs : Math.max(Math.floor(CPUs / 2), 1),
   collectCoverage: true,
   coverageReporters: process.env.CI ? ["text", "lcov"] : ["text"],
   coverageThreshold: {
--- jest.e2e.config.js
+++ jest.e2e.config.js
@@ -1,10 +1,13 @@
 /* Config file to run just end-to-end tests */
+const CPUs = require("os").cpus().length;
 
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testRegex: "e2e/node/.*.test.ts",
   clearMocks: true,
+  // Increase workers on CI:
+  maxWorkers: process.env.CI ? CPUs : Math.max(Math.floor(CPUs / 2), 1),
   // Increase timeout to accomodate variable network latency
   testTimeout: 30000,
   injectGlobals: false,
