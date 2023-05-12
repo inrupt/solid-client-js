dcafb1b9c4596f36bfcd1e53c5f70a8583281ce6
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-05-12 18:26:27 +0200

    chore: add read permission test with externally modified access

--- e2e/node/acp.test.ts
+++ e2e/node/acp.test.ts
@@ -37,7 +37,8 @@ import {
   getPodRoot,
   createFetch,
 } from "@inrupt/internal-test-env";
-import { acp_v4 as acp, getSolidDataset, getSourceUrl } from "../../src/index";
+import Link from "http-link-header";
+import { acp_v4 as acp, acp_ess_2, fromRdfJsDataset, getSolidDataset, getSourceUrl, saveSolidDatasetAt, toRdfJsDataset } from "../../src/index";
 import { getAccessControlUrlAll } from "../../src/acp/accessControl/getAccessControlUrlAll";
 import { getAgentAccess } from "../../src/universal/getAgentAccess";
 import { setAgentAccess } from "../../src/universal/setAgentAccess";
@@ -48,6 +49,7 @@ import {
   getPublicAccess as legacy_getPublicAccess,
 } from "../../src/access/universal";
 import { hasAccessibleAcr } from "../../src/acp/acp";
+import { DataFactory } from "../../src/rdfjs.internal";
 
 const TEST_SLUG = "solid-client-test-e2e-acp";
 
@@ -56,6 +58,21 @@ const env = getNodeTestingEnvironment();
 if (env?.features?.acp !== true) {
   // eslint-disable-next-line jest/no-focused-tests
   test.only(`Skipping unsupported ACP tests in ${env.environment}`, () => {});
+}
+
+const READ_ONLY_ACCESS = {
+  read: true,
+  append: false,
+  write: false,
+  controlRead: false,
+  controlWrite: false,
+}
+const APPEND_ONLY_ACCESS = {
+  read: false,
+  append: true,
+  write: false,
+  controlRead: false,
+  controlWrite: false,
 }
 
 describe("An ACP Solid server", () => {
@@ -124,22 +141,10 @@ describe("An ACP Solid server", () => {
       { read: true },
       fetchOptions
     );
-    expect(agentAccess).toStrictEqual({
-      read: true,
-      append: false,
-      write: false,
-      controlRead: false,
-      controlWrite: false,
-    });
+    expect(agentAccess).toStrictEqual(READ_ONLY_ACCESS);
     expect(
       await getAgentAccess(sessionResource, agent, fetchOptions)
-    ).toStrictEqual({
-      read: true,
-      append: false,
-      write: false,
-      controlRead: false,
-      controlWrite: false,
-    });
+    ).toStrictEqual(READ_ONLY_ACCESS);
   });
 
   it("can get and set access for multiple agents", async () => {
@@ -153,29 +158,24 @@ describe("An ACP Solid server", () => {
     );
     const bobAccess = await setAgentAccess(
       sessionResource,
-      alice,
+      bob,
       { append: true },
       fetchOptions
     );
-    expect(bobAccess).toStrictEqual({
-      read: false,
+    expect(bobAccess).toStrictEqual(APPEND_ONLY_ACCESS);
+    expect(aliceAccess).toStrictEqual(READ_ONLY_ACCESS);
+    expect(await getAgentAccess(sessionResource, alice, fetchOptions)).toStrictEqual(READ_ONLY_ACCESS);
+    expect(await getAgentAccess(sessionResource, bob, fetchOptions)).toStrictEqual(APPEND_ONLY_ACCESS);
+
+    const bobUpdatedAccess = await setAgentAccess(
+      sessionResource,
+      bob,
+      { read: true },
+      fetchOptions
+    );
+    expect(bobUpdatedAccess).toStrictEqual({
+      read: true,
       append: true,
-      write: false,
-      controlRead: false,
-      controlWrite: false,
-    });
-    expect(aliceAccess).toStrictEqual({
-      read: true,
-      append: false,
-      write: false,
-      controlRead: false,
-      controlWrite: false,
-    });
-    expect(
-      await getAgentAccess(sessionResource, alice, fetchOptions)
-    ).toStrictEqual({
-      read: true,
-      append: false,
       write: false,
       controlRead: false,
       controlWrite: false,
@@ -183,35 +183,66 @@ describe("An ACP Solid server", () => {
     expect(
       await getAgentAccess(sessionResource, bob, fetchOptions)
     ).toStrictEqual({
-      read: false,
+      read: true,
       append: true,
+      write: false,
+      controlRead: false,
+      controlWrite: false,
+    });
+  });
+
+  it("can get access for multiple agents with externally modified access", async () => {
+    const alice = "https://example.org/alice";
+    const bob = "https://example.org/bob";
+    const agent = DataFactory.namedNode("http://www.w3.org/ns/solid/acp#agent");
+    
+    const aliceAccess = await setAgentAccess(
+      sessionResource,
+      alice,
+      { append: true },
+      fetchOptions
+    );
+    expect(aliceAccess).toStrictEqual(APPEND_ONLY_ACCESS);
+    expect(await getAgentAccess(sessionResource, alice, fetchOptions)).toStrictEqual(APPEND_ONLY_ACCESS);
+    expect(
+      await getAgentAccess(sessionResource, bob, fetchOptions)
+    ).toStrictEqual({
+      read: false,
+      append: false,
       write: false,
       controlRead: false,
       controlWrite: false,
     });
 
-    const bobUpdatedAccess = await setAgentAccess(
-      sessionResource,
-      alice,
-      { read: true },
-      fetchOptions
-    );
-    expect(bobUpdatedAccess).toStrictEqual({
-      read: false,
-      append: true,
-      write: false,
-      controlRead: false,
-      controlWrite: false,
+    const headResponse = await session.fetch(sessionResource, {
+      method: "HEAD",
     });
-    expect(
-      await getAgentAccess(sessionResource, bob, fetchOptions)
-    ).toStrictEqual({
-      read: true,
-      append: true,
-      write: false,
-      controlRead: false,
-      controlWrite: false,
-    });
+
+    const responseLinks = headResponse.headers.get("Link")
+    if (!responseLinks) {
+      throw new Error("No Link header found");
+    }
+
+    const links = Link.parse(responseLinks.toString());
+    const acrUrl = links?.get("rel", "acl")[0].uri;
+    const acrDataset = await getSolidDataset(acrUrl, { fetch: session.fetch });
+    const rdfjs = toRdfJsDataset(acrDataset);
+
+    // Find the policies applying to alice and also apply them to bob
+    for (const p of rdfjs.match(null, agent, DataFactory.namedNode(alice))) {
+      rdfjs.add(DataFactory.quad(p.subject, agent, DataFactory.namedNode(bob)));
+    }
+
+    const datasetWithAcr = await acp_ess_2.getSolidDatasetWithAcr(sessionResource, { fetch: session.fetch });
+
+    if (!datasetWithAcr.internal_acp.acr) {
+      throw new Error("No ACR found");
+    }
+
+    await saveSolidDatasetAt(acrUrl, fromRdfJsDataset(rdfjs), { fetch: session.fetch });
+
+    expect(await getAgentAccess(sessionResource, alice, fetchOptions)).toStrictEqual(APPEND_ONLY_ACCESS);
+    expect(await getAgentAccess(sessionResource, bob, fetchOptions)).toStrictEqual(APPEND_ONLY_ACCESS);
   });
 
   it("can get and set read access for the public", async () => {
