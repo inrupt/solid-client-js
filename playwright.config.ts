1f23a1e734e46af80557e0225371416baef2427e
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2024-03-27 23:14:37 +0100

    fixup! Re-enable webkit tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -60,8 +60,6 @@ const config: PlaywrightTestConfig = {
         }. Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36`,
       },
     },
-    // WebKit currently cannot be tested as ESS is returning multiple Location headers which chokes WebKit
-    //
     {
       name: "WebKit",
       use: {
