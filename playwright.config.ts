c9ee58f08fb02c81547fc0fca72645b18ae129e9
Nicolas Ayral Seydoux <nseydoux@inrupt.com>
2024-03-27 23:35:41 +0100

    Re-skip playwright tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -60,15 +60,16 @@ const config: PlaywrightTestConfig = {
         }. Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36`,
       },
     },
-    {
-      name: "WebKit",
-      use: {
-        browserName: "webkit",
-        userAgent: `Browser-based solid-client end-to-end tests running ${
-          process.env.CI === "true" ? "in CI" : "locally"
-        }. Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1`,
-      },
-    },
+    // There are currently issues making the Webkit tests flaky on playwright, even using version 1.37.X.
+    // {
+    //   name: "WebKit",
+    //   use: {
+    //     browserName: "webkit",
+    //     userAgent: `Browser-based solid-client end-to-end tests running ${
+    //       process.env.CI === "true" ? "in CI" : "locally"
+    //     }. Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1`,
+    //   },
+    // },
   ],
 };
 
